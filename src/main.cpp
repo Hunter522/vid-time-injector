2aede6cb25131e85777ffb5c74a60534ba2dfe43
Hunter N. Morgan <hunter.morgan@tamucc.edu>
2016-08-06 19:32:31 -0500

    Bitstream filter now works, remuxing to MPEG-TS is now working

--- src/main.cpp
+++ src/main.cpp
@@ -1,181 +1,26 @@
-// /*
-//  * Copyright (c) 2013 Stefano Sabatini
-//  *
-//  * Permission is hereby granted, free of charge, to any person obtaining a copy
-//  * of this software and associated documentation files (the "Software"), to deal
-//  * in the Software without restriction, including without limitation the rights
-//  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  * copies of the Software, and to permit persons to whom the Software is
-//  * furnished to do so, subject to the following conditions:
-//  *
-//  * The above copyright notice and this permission notice shall be included in
-//  * all copies or substantial portions of the Software.
-//  *
-//  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-//  * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  * THE SOFTWARE.
-//  */
-// /**
-//  * @file
-//  * libavformat/libavcodec demuxing and muxing API example.
-//  *
-//  * Remux streams from one container format to another.
-//  * @example remuxing.c
-//  */
-//  #include <getopt.h>
-//  #include <stdio.h>
-//  #include <string>
-//  #include <iostream>
-//  #include <fstream>
-//  #include <log4cpp/Category.hh>
-//  #include <log4cpp/FileAppender.hh>
-//  #include <log4cpp/OstreamAppender.hh>
-//
-//  extern "C" {
-//  #include "libavcodec/avcodec.h"
-//  #include "libavformat/avformat.h"
-//  #include <libavutil/common.h>
-//  #include "libavutil/dict.h"
-//  #include <libavutil/imgutils.h>
-//  #include <libavutil/samplefmt.h>
-//  #include <libavutil/timestamp.h>
-//  #include <libavutil/opt.h>
-//  #include <libavutil/channel_layout.h>
-//  #include <libavutil/mathematics.h>
-//  #include <libavfilter/avfilter.h>
-//  }
-//  #define ts2str(ts) av_ts_make_string(new char[AV_TS_MAX_STRING_SIZE], ts)
-//  #define ts2timestr(ts, tb) av_ts_make_time_string(new char[AV_TS_MAX_STRING_SIZE], ts, tb)
-//
-//
-//  static void log_packet(const AVFormatContext *fmt_ctx, const AVPacket *pkt, const char *tag) {
-//      AVRational* time_base = &fmt_ctx->streams[pkt->stream_index]->time_base;
-//
-//      char* pts = ts2str(pkt->pts);
-//      char* pts_time_base = ts2timestr(pkt->pts, time_base);
-//      char* dts = ts2str(pkt->dts);
-//      char* dts_time_base = ts2timestr(pkt->dts, time_base);
-//      char* duration = ts2str(pkt->duration);
-//      char* duration_time_base = ts2timestr(pkt->duration, time_base);
-//      printf("%s: pts:%s pts_time:%s dts:%s dts_time:%s duration:%s duration_time:%s stream_index:%d\n",
-//             tag,
-//             pts, pts_time_base,
-//             dts, dts_time_base,
-//             duration, duration_time_base,
-//             pkt->stream_index);
-//      delete pts;
-//      delete pts_time_base;
-//      delete dts;
-//      delete dts_time_base;
-//      delete duration;
-//      delete duration_time_base;
-//  }
-// int main(int argc, char **argv)
-// {
-//     AVOutputFormat *ofmt = NULL;
-//     AVFormatContext *ifmt_ctx = NULL, *ofmt_ctx = NULL;
-//     AVPacket pkt;
-//     const char *in_filename, *out_filename;
-//     int ret, i;
-//     if (argc < 3) {
-//         printf("usage: %s input output\n"
-//                "API example program to remux a media file with libavformat and libavcodec.\n"
-//                "The output format is guessed according to the file extension.\n"
-//                "\n", argv[0]);
-//         return 1;
-//     }
-//     in_filename  = argv[1];
-//     out_filename = argv[2];
-//     av_register_all();
-//     if ((ret = avformat_open_input(&ifmt_ctx, in_filename, 0, 0)) < 0) {
-//         fprintf(stderr, "Could not open input file '%s'", in_filename);
-//         goto end;
-//     }
-//     if ((ret = avformat_find_stream_info(ifmt_ctx, 0)) < 0) {
-//         fprintf(stderr, "Failed to retrieve input stream information");
-//         goto end;
-//     }
-//     av_dump_format(ifmt_ctx, 0, in_filename, 0);
-//     avformat_alloc_output_context2(&ofmt_ctx, NULL, NULL, out_filename);
-//     if (!ofmt_ctx) {
-//         fprintf(stderr, "Could not create output context\n");
-//         ret = AVERROR_UNKNOWN;
-//         goto end;
-//     }
-//     ofmt = ofmt_ctx->oformat;
-//     for (i = 0; i < ifmt_ctx->nb_streams; i++) {
-//         AVStream *in_stream = ifmt_ctx->streams[i];
-//         AVStream *out_stream = avformat_new_stream(ofmt_ctx, in_stream->codec->codec);
-//         if (!out_stream) {
-//             fprintf(stderr, "Failed allocating output stream\n");
-//             ret = AVERROR_UNKNOWN;
-//             goto end;
-//         }
-//         ret = avcodec_copy_context(out_stream->codec, in_stream->codec);
-//         if (ret < 0) {
-//             fprintf(stderr, "Failed to copy context from input to output stream codec context\n");
-//             goto end;
-//         }
-//         out_stream->codec->codec_tag = 0;
-//         if (ofmt_ctx->oformat->flags & AVFMT_GLOBALHEADER)
-//             out_stream->codec->flags |= CODEC_FLAG_GLOBAL_HEADER;
-//     }
-//     av_dump_format(ofmt_ctx, 0, out_filename, 1);
-//     if (!(ofmt->flags & AVFMT_NOFILE)) {
-//         ret = avio_open(&ofmt_ctx->pb, out_filename, AVIO_FLAG_WRITE);
-//         if (ret < 0) {
-//             fprintf(stderr, "Could not open output file '%s'", out_filename);
-//             goto end;
-//         }
-//     }
-//     ret = avformat_write_header(ofmt_ctx, NULL);
-//     if (ret < 0) {
-//         fprintf(stderr, "Error occurred when opening output file\n");
-//         goto end;
-//     }
-//     while (1) {
-//         AVStream *in_stream, *out_stream;
-//         ret = av_read_frame(ifmt_ctx, &pkt);
-//         if (ret < 0)
-//             break;
-//         in_stream  = ifmt_ctx->streams[pkt.stream_index];
-//         out_stream = ofmt_ctx->streams[pkt.stream_index];
-//         log_packet(ifmt_ctx, &pkt, "in");
-//         /* copy packet */
-//         pkt.pts = av_rescale_q_rnd(pkt.pts, in_stream->time_base, out_stream->time_base, (AVRounding)(AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
-//         pkt.dts = av_rescale_q_rnd(pkt.dts, in_stream->time_base, out_stream->time_base, (AVRounding)(AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX));
-//         pkt.duration = av_rescale_q(pkt.duration, in_stream->time_base, out_stream->time_base);
-//         pkt.pos = -1;
-//         log_packet(ofmt_ctx, &pkt, "out");
-//         ret = av_interleaved_write_frame(ofmt_ctx, &pkt);
-//         if (ret < 0) {
-//             fprintf(stderr, "Error muxing packet\n");
-//             break;
-//         }
-//         av_free_packet(&pkt);
-//     }
-//     av_write_trailer(ofmt_ctx);
-// end:
-//     avformat_close_input(&ifmt_ctx);
-//     /* close output */
-//     if (ofmt_ctx && !(ofmt->flags & AVFMT_NOFILE))
-//         avio_closep(&ofmt_ctx->pb);
-//     avformat_free_context(ofmt_ctx);
-//     if (ret < 0 && ret != AVERROR_EOF) {
-//         fprintf(stderr, "Error occurred: bad stuff\n");
-//         return 1;
-//     }
-//     return 0;
-// }
-
-
-
-
 /*
+                   ██╗   ██╗██╗██████╗
+                   ██║   ██║██║██╔══██╗
+                   ██║   ██║██║██║  ██║
+                   ╚██╗ ██╔╝██║██║  ██║
+                    ╚████╔╝ ██║██████╔╝
+                     ╚═══╝  ╚═╝╚═════╝
+
+              ████████╗██╗███╗   ███╗███████╗
+              ╚══██╔══╝██║████╗ ████║██╔════╝
+                 ██║   ██║██╔████╔██║█████╗
+                 ██║   ██║██║╚██╔╝██║██╔══╝
+                 ██║   ██║██║ ╚═╝ ██║███████╗
+                 ╚═╝   ╚═╝╚═╝     ╚═╝╚══════╝
+
+██╗███╗   ██╗     ██╗███████╗ ██████╗████████╗ ██████╗ ██████╗
+██║████╗  ██║     ██║██╔════╝██╔════╝╚══██╔══╝██╔═══██╗██╔══██╗
+██║██╔██╗ ██║     ██║█████╗  ██║        ██║   ██║   ██║██████╔╝
+██║██║╚██╗██║██   ██║██╔══╝  ██║        ██║   ██║   ██║██╔══██╗
+██║██║ ╚████║╚█████╔╝███████╗╚██████╗   ██║   ╚██████╔╝██║  ██║
+╚═╝╚═╝  ╚═══╝ ╚════╝ ╚══════╝ ╚═════╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝
+
+
  * Consumes a video stream, transmuxes to MPEG2-TS, and injects a timestamp
  * in the data stream of the MPEG2-TS.
  *
@@ -274,7 +119,7 @@ void usage() {
 
 int main (int argc, char **argv) {
     // INPUT
-    AVFormatContext *       input_fmt_ctx = NULL;         // Input Format context
+    AVFormatContext*        input_fmt_ctx = NULL;   // Input Format context
     int                     input_vid_stream_idx;
     // OUTPUT
     AVOutputFormat*         output_fmt = NULL;      // output format
@@ -455,23 +300,22 @@ int main (int argc, char **argv) {
         log_packet(output_fmt_ctx, &pkt, "out");
 
         if(pkt.stream_index == input_vid_stream_idx) {
+            // if frame is video
             // filter packet using the h264_mp4toannexb bitstream filter
+            // then write frame to output_fmt_ctx
             AVPacket filtered_pkt = pkt;
 
             logger.debug("Filtering...");
-            int f = pkt.flags & AV_PKT_FLAG_KEY;
+            const int f = pkt.flags & AV_PKT_FLAG_KEY;
 
-
-            // FIXME I think the problem why its segfauulting here is because the 2nd and 3rd arg
-            // are needed...cant be null
             ret = av_bitstream_filter_filter(h264_mp4toannexb_filter_ctx,     // bitstream filter context
-                                             NULL,                            // AVCodecContext, may be NULL
+                                             out_stream->codec,               // output stream encoder AVCodecContext, may be NULL
                                              NULL,                            // filter config args, may be NULL
                                              &filtered_pkt.data,              // output pointer to filter buffer
                                              &filtered_pkt.size,              // output pointer to filter buffer len
                                              pkt.data,                        // input buffer
                                              pkt.size,                        // input buffer size
-                                             f);    // if input buf corresponds to key-frame pkt data
+                                             f);                              // if input buf corresponds to key-frame pkt data
 
             if(ret < 0) {
                 logger.error("Error filtering packet");
@@ -489,11 +333,11 @@ int main (int argc, char **argv) {
             logger.debug("Writing filtered frame...");
             ret = av_interleaved_write_frame(output_fmt_ctx, &filtered_pkt);
         } else {
+            // else
+            // write write frame to output_fmt_ctx without filtering
             logger.debug("Writing frame...");
             ret = av_interleaved_write_frame(output_fmt_ctx, &pkt);
         }
-
-
 
         if(ret < 0) {
             logger.error("Error muxing packet");
@@ -504,28 +348,9 @@ int main (int argc, char **argv) {
     }
     av_write_trailer(output_fmt_ctx);
 
-
-
-
-
-//     // Read frames from input
-//     logger.debug("Reading frames...");
-//     while (av_read_frame(fmt_ctx, &pkt) >= 0) {
-//         if (pkt.stream_index == vid_idx_stream) {         // packet from video stream
-//             //printf("Parsing video packet pos = %d\n", pkt.pos);
-// //            avcodec_decode_video2(vid_dec_ctx, in_frame, &fF, &pkt);
-// //            sws_scale(cvt_ctx, in_frame->data,
-// //                      in_frame->linesize, 0,
-// //                      vid_dec_ctx->height,
-// //                      out_frame->data,
-// //                      out_frame->linesize);
-// //            sleep_ms = pkt.duration / 90;
-//             vid_pkt_cnt++;
-//         }
-//
-//         av_free_packet(&pkt);
-//     }
-
+    ///////////////
+    // CLEAN UP
+    ///////////////
     logger.debug("Cleaning up...");
     logger.debug("read %ld video packets", vid_pkt_cnt);
     logger.debug("processed %ld frames", frame_ctr);
